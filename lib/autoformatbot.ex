471d8e047089ababa57e420407bd178b30de8009
autoformatbot <autoformatbot@example.com>
2018-10-07 00:20:44 +0200

    autoformat lib/autoformatbot.ex

--- lib/autoformatbot.ex
+++ lib/autoformatbot.ex
@@ -15,10 +15,13 @@ defmodule Autoformatbot do
       &autoformat_enabled_for_branch?/1,
       &needs_formatting?/1,
       {:files, &format!/1},
-      {:target_branch, fn %{config: %{suffix: s}, current_branch: b} -> {:ok, b <> s} end},
+      {:target_branch, &target_branch/1},
+      {:temporary_branch, &temporary_branch/1},
       {:adapter, &init_adapter/1},
-      &prepare_target_branch/1,
-      &upload_files/1,
+      &prepare_temporary_branch/1,
+      {:target_sha, &update_files/1},
+      &create_or_reset_target_branch/1,
+      &delete_temporary_branch/1,
       &create_pull/1
     ]
     |> Token.pipeline()
@@ -51,6 +54,12 @@ defmodule Autoformatbot do
     end
   end
 
+  defp target_branch(%{config: %{suffix: s}, current_branch: b}),
+    do: {:ok, b <> s}
+
+  defp temporary_branch(%{config: %{suffix: s}, current_branch: b}),
+    do: {:ok, b <> s <> (DateTime.utc_now() |> DateTime.to_unix() |> Integer.to_string())}
+
   defp format!(_token) do
     with {:ok, _} <- cmd("mix", ["format"]),
          {:ok, output} <- cmd!("git", ["diff", "--name-only"]) do
@@ -65,7 +74,20 @@ defmodule Autoformatbot do
     end
   end
 
-  defp prepare_target_branch(%{adapter: {mod, c}, current_sha: sha, target_branch: b}) do
+  defp prepare_temporary_branch(%{adapter: {mod, c}, current_sha: sha, temporary_branch: b}) do
+    mod.create_branch!(c, b, sha)
+  end
+
+  defp update_files(%{adapter: {mod, c}, files: files, target_branch: b}) do
+    Enum.reduce_while(files, :ok, fn path, _acc ->
+      case mod.update_file!(c, b, path) do
+        {:ok, sha} -> {:cont, {:ok, sha}}
+        err -> {:halt, err}
+      end
+    end)
+  end
+
+  defp create_or_reset_target_branch(%{adapter: {mod, c}, target_branch: b, target_sha: sha}) do
     with {:ok, branch_exists} <- mod.branch_exists?(c, b) do
       if branch_exists do
         mod.reset_branch!(c, b, sha)
@@ -75,14 +97,8 @@ defmodule Autoformatbot do
     end
   end
 
-  defp upload_files(%{adapter: {mod, c}, files: files, target_branch: b}) do
-    Enum.reduce_while(files, :ok, fn file, _acc ->
-      case mod.update_file!(c, file, b) do
-        :ok -> {:cont, :ok}
-        err -> {:halt, err}
-      end
-    end)
-  end
+  defp delete_temporary_branch(%{adapter: {mod, c}, temporary_branch: b}),
+    do: mod.remove_branch!(c, b)
 
   defp create_pull(%{adapter: {mod, c}, current_branch: base, target_branch: head}),
     do: mod.create_pull!(c, base, head)
