540d10ee0b0b799886472450f0e824c83894e36c
autoformatbot <autoformatbot@example.com>
2018-10-08 11:13:05 +0200

    autoformat config/test.exs

--- config/test.exs
+++ config/test.exs
@@ -1,8 +1,5 @@
 use Mix.Config
 
 config :autoformatbot,
-  adapter: {:github,
-    owner: "maltoe",
-    repo: "autoformatbot.ex",
-    access_token: {:system, "GITHUB_TOKEN"}
-  }
+  adapter:
+    {:github, owner: "maltoe", repo: "autoformatbot.ex", access_token: {:system, "GITHUB_TOKEN"}}
