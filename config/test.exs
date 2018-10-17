76d074822f753f8f822076071427500813d340b3
autoformatbot <autoformatbot@example.com>
2018-10-17 17:46:37 +0200

    [ci skip] autoformat config/test.exs

--- config/test.exs
+++ config/test.exs
@@ -1,14 +1,11 @@
 use Mix.Config
 
 config :autoformatbot,
-  adapter: {:github,
-    owner: "maltoe",
-    repo: "autoformatbot.ex",
-    access_token: {:system, "GITHUB_TOKEN"}
-  },
+  adapter:
+    {:github, owner: "maltoe", repo: "autoformatbot.ex", access_token: {:system, "GITHUB_TOKEN"}},
   current_sha: {:system, "TRAVIS_COMMIT"},
-  current_branch: (fn ->
+  current_branch: fn ->
     if System.get_env("TRAVIS_PULL_REQUEST") == "false",
       do: System.get_env("TRAVIS_BRANCH"),
-    else: System.get_env("TRAVIS_PULL_REQUEST_BRANCH")
-  end)
+      else: System.get_env("TRAVIS_PULL_REQUEST_BRANCH")
+  end
